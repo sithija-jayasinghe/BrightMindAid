58a5a0f4fa1ad76cedd0106f7d5ffbf1f5726193
sithija-jayasinghe <harshanajayasinghe113@gmail.com>
2026-01-31 13:13:26 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BrightMindAid/sw.js', { scope: '/BrightMindAid/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
